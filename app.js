0d11516cbdbdecde81dd69851bf1e484659245db
Kenzo Nagari <kenzonagari@gmail.com>
2022-07-26 12:58:42 -0400

    commit 2: set win and lose condition

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 $(() => {
     
-const testWord = 'apple';
+const testWord = 'apple'//WORDS[Math.floor(Math.random()*WORDS.length)];
 let inputWord = [];
 let turnCounter = 1;
 let hasInput = false;
@@ -18,37 +18,6 @@ function printWord (string) {
 
 }
 
-function compareWord () {
-    const testWordDouble = [];
-
-    for(letter of testWord){
-        testWordDouble.push(letter.toUpperCase());
-    }
-
-    for (let i = 0 ; i < 5 ; i++){
-        $(`#letter${i}`).css('background', '#454545');
-        
-        for (let j = 0; j < 5 ; j++){
-            if(inputWord[i] === testWordDouble[j]){ //if letter exists but not in correct square
-                $(`#letter${i}`).css('background', '#b59f3b');
-
-                if(i===j){
-                    $(`#letter${i}`).css('background', '#538d4e'); //if letter is in correct square
-                }
-                
-                testWordDouble[j] = ""; //replace the common letter with a blank
-                j = 4; //force end loop
-            }
-        }
-        
-        $(`#letter${i}`).attr('id', 'locked');        
-    }
-
-    inputWord = [];
-    hasInput = false;
-    console.log(testWordDouble);
-}
-
 function checkDictionary(word){
     for(let i = 0 ; i < WORDS.length ; i++){
         if(word === WORDS[i]){
@@ -58,9 +27,54 @@ function checkDictionary(word){
     return false;
 }
 
+function compareWord (turn) {
+    let correctLetter = 0;
+    const testWordDouble = [];
+    
+    for(letter of testWord){
+        testWordDouble.push(letter.toUpperCase());
+    }
+    
+    for (let i = 0 ; i < 5 ; i++){
+        
+        $(`#letter${i}`).css('background', '#454545'); //grey
+        
+        for (let j = 0; j < 5 ; j++){
+            if(inputWord[i] === testWordDouble[j]){ //if letter exists but not in correct square
+                $(`#letter${i}`).css('background', '#b59f3b'); //yellow
 
+                if(i===j){ //if letter is in correct square
+                    $(`#letter${i}`).css('background', '#538d4e'); //green
+                    correctLetter++;
+                }
+                
+                testWordDouble[j] = ""; //replace the common letter with a blank
+                j = 4; //force end loop
+            }
+        }
+        
+        $(`#letter${i}`).attr('id', 'locked');        
+    }
+    
+    inputWord = [];
+    hasInput = false;
+    console.log(testWordDouble, turn);
+    if(correctLetter === 5){
+        win();
+    }
+}
 
-function createRow (turn) {
+function win(){
+    $('form').hide();
+    $('#message-log').text('You guessed the word!');
+}
+
+function lose (){
+    $('form').hide();
+    $('#message-log').text('Try again next time!');
+}
+
+function toggleRow (turn) {
     $(`#row${turn}`).empty();
     if(turn < 6){
         for(let i = 0 ; i < 5 ; i++){
@@ -71,7 +85,7 @@ function createRow (turn) {
 }
 
 $("form").on("submit", (event) => {
-    
+
     event.preventDefault();
     const string = $("#input-box").val();
     
@@ -79,17 +93,21 @@ $("form").on("submit", (event) => {
         printWord(string);
         hasInput = true;
     } else {
-        console.log("fuck off");
+        console.log("Invalid word!");
     }
 
     $(event.currentTarget).trigger("reset");
+
 });
 
 $('#check').on("click", ()=>{
     if(hasInput){
-        compareWord();
-        createRow(turnCounter);
+        compareWord(turnCounter);
+        toggleRow(turnCounter);
         turnCounter++
+        if(turnCounter > 6){
+            lose();
+        }
     }
 })
 
